68000360556150d30b22605ed8fc1a7671dfec7b
luiz eduardo silva <luizscarpo@gmail.com>
2021-01-07 01:17:23 -0300

    Add LoginPage

---
+++ lib/app/pages/splash/splash_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class SplashPage extends StatefulWidget {
+  final String title;
+  const SplashPage({Key key, this.title = "Splash"}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 2)).then((v) {
+      Modular.to.pushReplacementNamed('/home/');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
---
+++ test/app/pages/splash/splash_controller_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:open_travel_desafio/app/pages/splash/splash_controller.dart';
+import 'package:open_travel_desafio/app/app_module.dart';
+
+void main() {
+  initModule(AppModule());
+  // SplashController splash;
+  //
+  setUp(() {
+    //     splash = AppModule.to.get<SplashController>();
+  });
+
+  group('SplashController Test', () {
+    //   test("First Test", () {
+    //     expect(splash, isInstanceOf<SplashController>());
+    //   });
+
+    //   test("Set Value", () {
+    //     expect(splash.value, equals(0));
+    //     splash.increment();
+    //     expect(splash.value, equals(1));
+    //   });
+  });
+}
