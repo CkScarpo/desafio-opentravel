a9d9b92ff55932d786ead2c9dfa6e079c63ce79c
luiz eduardo silva <luizscarpo@gmail.com>
2021-01-07 01:14:40 -0300

    Update AppModule

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,21 +1,27 @@
+import 'package:open_travel_desafio/app/pages/splash/splash_controller.dart';
 import 'package:open_travel_desafio/app/app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:open_travel_desafio/app/app_widget.dart';
+import 'package:open_travel_desafio/app/pages/splash/splash_page.dart';
 
 import 'modules/home/home_controller.dart';
 import 'modules/home/home_module.dart';
+import 'modules/login/login_module.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
+        Bind((i) => SplashController()),
         Bind((i) => AppController()),
         Bind((i) => HomeController()),
       ];
 
   @override
   List<ModularRouter> get routers => [
-        ModularRouter('/', module: HomeModule()),
+        ModularRouter('/', child: (_, args) => SplashPage()),
+        ModularRouter('/login', module: LoginModule()),
+        ModularRouter('/home', module: HomeModule()),
       ];
 
   @override
